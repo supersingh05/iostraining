5fa07a2f3b91559784490cdea1a748a112afcfe7
Asavir Kalla <asavirkalla@Asavirs-MBP.attlocal.net>
2016-06-24 22:33:16 -0700

    variables in swift

---
+++ variables.playground/Contents.swift
@@ -0,0 +1,42 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+// variable is an object that stores or refers to values
+//values in constants cannot change
+
+// example of constant
+let maximumNumberOfWithdrawals = 5
+
+//example of variable 
+var withdrawalAttempts = 3
+
+//creating multiple constants/vars in one line
+var a = 2, b = 0, c = 1
+
+let d = 2.2, e = 1.1, f = 3.3
+
+//type annotation , define its type
+// declaring variable of type string
+var hellowSwift2: String
+
+//can define related variable of same time in same line
+var north, south, east, west: String
+
+//swift has type inference
+var someString = "this is type inference, so makes this a string"
+
+// can have variables of emojis names, any unicode chars, but no whitespace or nums in start
+
+let 💩 = "emoji constant lulz"
+
+var hola = "Hello"
+hola = "hi"
+
+
+//print function, prints to console
+
+print(hola)
+
